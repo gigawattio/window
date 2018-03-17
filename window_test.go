5bb0469d1959af48e8622ac1c7c7558e65d5d4d3
Jay Taylor <outtatime@gmail.com>
2018-03-17 12:09:26 -0700

    Initial commit.

---
+++ window_test.go
@@ -0,0 +1,84 @@
+package window
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestWindow(t *testing.T) {
+	testCases := []struct {
+		elements []string
+		n        int
+		expected [][]string
+	}{
+		{
+			elements: nil,
+			n:        0,
+			expected: nil,
+		},
+		{
+			elements: []string{},
+			n:        0,
+			expected: nil,
+		},
+		{
+			elements: []string{"bail", "early"},
+			n:        -1,
+			expected: nil,
+		},
+		{
+			elements: []string{"foo", "bar", "baz"},
+			n:        0,
+			expected: nil,
+		},
+		{
+			elements: []string{"foo", "bar", "baz"},
+			n:        1,
+			expected: [][]string{
+				[]string{"foo"},
+				[]string{"bar"},
+				[]string{"baz"},
+			},
+		},
+		{
+			elements: []string{"foo", "bar", "baz"},
+			n:        1,
+			expected: [][]string{
+				[]string{"foo"},
+				[]string{"bar"},
+				[]string{"baz"},
+			},
+		},
+		{
+			elements: []string{"foo", "bar", "baz"},
+			n:        2,
+			expected: [][]string{
+				[]string{"foo", "bar"},
+				[]string{"bar", "baz"},
+			},
+		},
+		{
+			elements: []string{"foo", "bar", "baz"},
+			n:        3,
+			expected: [][]string{
+				[]string{"foo", "bar", "baz"},
+			},
+		},
+		{
+			elements: []string{"foo", "bar", "baz", "boo"},
+			n:        2,
+			expected: [][]string{
+				[]string{"foo", "bar"},
+				[]string{"bar", "baz"},
+				[]string{"baz", "boo"},
+			},
+		},
+	}
+
+	for i, testCase := range testCases {
+		actual := Window(testCase.elements, testCase.n)
+		if !reflect.DeepEqual(actual, testCase.expected) {
+			t.Errorf("[i=%v] Expected result did not match actual\n\tExpected: %+v\n\t  Actual: %+v", i, testCase.expected, actual)
+		}
+	}
+}
